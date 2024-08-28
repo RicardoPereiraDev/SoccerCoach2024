6fdf215d2e8db03740a4e00db6fe55c218280bf6
RicardoPereiraDev <odracirDavidPereira@gmail.com>
2024-08-28 01:07:51 +0100

    Project created

---
+++ backend/src/main/java/com/devsuperior/SoccerCoach202408/SoccerCoach202408Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.SoccerCoach202408;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerCoach202408Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerCoach202408Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/SoccerCoach202408/SoccerCoach202408ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.SoccerCoach202408;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerCoach202408ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
